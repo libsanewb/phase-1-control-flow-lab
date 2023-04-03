88ae2b050b979c9acc53f4ae7b2414e974cfdfb2
estifanos <libsanewb@gmail.com>
2023-04-02 23:25:30 -0400

    all test pase

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
+  if(num <= 400){
+  return 'This one is on me!';
+  }else if(num <= 2000){
+    return 'That will be twenty bucks.';
+  }else if(num <= 2500){
+    return 'I will gladly take your thirty bucks.';  
+    }else return 'No can do.';
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
+  if(cityName=='NYC')return 'Ok, sounds good.'
+  else return 'No go.';
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tips){
+  if(tips=='generous')return "Thank you so much.";
+  else if(tips=='not as generous')return 'Thank you.';
+  else return 'Bye.';
   // Write your code here!
 }
